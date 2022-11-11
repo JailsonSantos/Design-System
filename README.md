5ca35e158dbdbfb2047a4f1a2f601e0ce6ca6396
JailsonSantos <jailson.ads007@gmail.com>
2022-11-11 17:23:03 -0300

    Configurando o ESLINT

---
+++ README.md
@@ -0,0 +1,23 @@
+# Design-System
+Aplicando conceitos de Design System
+
+# Components
+- [ ] Text
+- [ ] Heading
+- [ ] Box
+- [ ] Button
+- [ ] TextInput
+- [ ] TextArea
+- [ ] Checkbox
+- [ ] Avatar
+- [ ] MultiSep
+
+
+Anotações
+- npm init -y (criar o package.json)
+- npm i -D typescript (instalar o TS)
+- npx tsc --init (criar o tsconfig.json)
+- npm i tsup -D (ferramenta que faz a conversão em um pacote JS, para ser utilizado em outras aplicações)
+  -- link da documentação: https://github.com/egoist/tsup
+
+- MonoRepo -> Conceito que sugere trabalhar com vários projetos que são dependentes um do outro em um único repositorio; ex: react + tokens
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,40 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    // Aqui é para manter o padrão do themeMap para as outras propriedades que não fiz de forma manual
+    ...defaultThemeMap,
+
+    // Isso é para quando for acessar o width e height recomendar o token space
+    width: 'space',
+    height: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
