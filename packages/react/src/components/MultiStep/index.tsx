53c8426ec16f0a3117f601d8042f32c043815814
JailsonSantos <jailson.ads007@gmail.com>
2022-11-14 16:50:26 -0300

    Componentes do Design System - Componente: MultiStep

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,21 @@
+import { Label, MultStepContainer, Steps, Step } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, indice) => indice + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultStepContainer>
+  )
+}
